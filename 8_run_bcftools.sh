478e81a03e4a1edd51cd146d4ca40d15a30ed3d3
acarolinao <98407281+acarolinao@users.noreply.github.com>
2026-01-15 17:25:04 +0000

    Delete 8_run_bcftools.sh

--- 8_run_bcftools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --mem 50G -c 8
-region=$1
-source bcftools-1.12
-
-bcftools mpileup --threads 8 -f Musa_acuminata_pahang_v4.fasta -r ${region} -a AD,DP -b list_bams.txt -Ou | \
-bcftools call --threads 8 -a GQ,GP --ploidy 2 -mv -Ou | \
-bcftools view --threads 8 -i '%QUAL>=30' -Oz -o calls_${region}.vcf.gz
-
